0309afb6499019de6c153afcc75da01fd8e2637f
RAMYA-1994 <ramyarrr2016@gmail.com>
2024-04-17 13:46:24 +0530

    some changes

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 
 function weatherData(){
-    var res=fetch`https://api.openweathermap.org/data/2.5/weather?q
-    =${cityName}&appid=7ba030a08ac1054ad66d68a246c26568`
-    console.log(res)
-let cityName=document.getElementsByClassName('cityName').value;
-res.then((data)=>data.json()).then((data1)=>(console.log(data1)))
-let weatherCard=document.getElementsByClassName('weatherCard');
+ 
+  let cityName=document.getElementById('cityName').value;
+  console.log(cityName)
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=chennai&appid=7ba030a08ac1054ad66d68a246c26568")
+  .then(res=>res.json()).then(data =>{
+
+let weatherCard=document.getElementById('weatherCard');
   weatherCard.innerHTML=`<div class="card" style="width: 18rem;">
-  <img src=https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos
-  -vectors%2Fweather&psig=AOvVaw1l3Py0g6u-8cn47-8Pt0DD&ust=1713117818094000&source=
-  images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDyiMzjv4UDFQAAAAAdAAAAABAE alt="weather image">
+  <img src="https://cdn5.vectorstock.com/i/1000x1000/68/34/abstract-day-weather-image-set-vector-4306834.jpg"
+   alt="weather image">
   <div class="card-body">
     <h5 class="card-title">weather Data</h5>
-    <p class="card-text">Temperature:${result.main.tempt}</p>
-    <p class="card-text">Windspeed:${result.wind.speed}</p>
-   
+    <p class="card-text">Temperature:${data.main.tempt}</p>
+    <p class="card-text">Windspeed:${data.wind.speed}</p>
+
    
   </div>
 </div>`
-
+ } )
 
 
 }
 
 
-weatherData()
+// weatherData()
 
